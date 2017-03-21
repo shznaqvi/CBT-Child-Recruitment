c61d52c9bd12ca5bd993735ffc04b5f44d609ff3
gul.sanober <gulsanobermemon@gmail.com>
2017-03-21 10:47:05 +0500

    All Activities added

---
+++ app/src/main/java/edu/aku/hassannaqvi/cbt_child_recruitment/SectionKActivity.java
@@ -0,0 +1,13 @@
+package edu.aku.hassannaqvi.cbt_child_recruitment;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SectionKActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_k);
+    }
+}
